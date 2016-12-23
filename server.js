0df21c72b2012d47dd6ada04f36e5f72304ab9b9
Simon Schwartz <simon.schwartz0@gmail.com>
2016-12-23 19:57:28 +1100

    implements travisCI auth web application flow with users GitHub api access token

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ function loadConfig() {
 
 var config = loadConfig();
 
-function authenticate(code, cb) {
+function authenticateGitHub(code, cb) {
   var data = qs.stringify({
     client_id: config.oauth_client_id,
     client_secret: config.oauth_client_secret,
@@ -49,11 +49,41 @@ function authenticate(code, cb) {
   req.on('error', function(e) { cb(e.message); });
 }
 
+function authenticateTravis(github_token, cb) {
+  var data = qs.stringify({
+    github_token: github_token
+  });
+
+  var reqOptions = {
+    host: config.travis_host,
+    port: config.oauth_port,
+    path: config.travis_auth_path,
+    method: config.oauth_method,
+    headers: {
+      'User-Agent': 'Travis/1.0.0'
+    }
+  };
+
+  var body = '';
+  var req = https.request(reqOptions, function(res) {
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) { body += chunk; });
+    res.on('end', function() {
+      var tokenString = body.match(':"(.*)"}');
+      cb(null, tokenString[1]); //this is our Travis API token
+    });
+  });
+
+  req.write(data);
+  req.end();
+  req.on('error', function(e) { cb(e.message); });
+}
+
 
 // Convenience for allowing CORS on routes - GET only
 app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*'); 
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS'); 
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
@@ -61,9 +91,17 @@ app.all('*', function (req, res, next) {
 
 app.get('/authenticate/:code', function(req, res) {
   console.log('authenticating code:' + req.params.code);
-  authenticate(req.params.code, function(err, token) {
+  authenticateGitHub(req.params.code, function(err, token) {
     var result = err || !token ? {"error": "bad_code"} : { "token": token };
     console.log(result);
+    res.json(result);
+  });
+});
+
+app.get('/auth/travis/:github_token', function(req, res) {
+  authenticateTravis(req.params.github_token, function(err, access_token) {
+    var result = err || !access_token ? {"error": "bad_github_token"} : { access_token };
+    console.log(result)
     res.json(result);
   });
 });
