c04cf5453e2a8f1c1d4ee964c981ff4956e05225
Simon Schwartz <simon.schwartz0@gmail.com>
2016-12-25 20:37:02 +1100

    redeclare server.listen

--- server.js
+++ server.js
@@ -106,8 +106,5 @@ app.get('/auth/travis/:github_token', function(req, res) {
   });
 });
 
-var port = process.env.PORT || config.port || 9999;
-
-app.listen(port, null, function (err) {
-  console.log('Gatekeeper, at your service: http://localhost:' + port);
-});
+var port_number = server.listen(process.env.PORT || 9999);
+app.listen(port_number);
