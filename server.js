6cc88421939bc30912ea42b8280babf83f67f3a8
Simon Schwartz <simon.schwartz0@gmail.com>
2016-12-26 15:31:38 +1100

    revert function names

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ function loadConfig() {
 
 var config = loadConfig();
 
-function authenticateGitHub(code, cb) {
+function authenticate(code, cb) {
   var data = qs.stringify({
     client_id: config.oauth_client_id,
     client_secret: config.oauth_client_secret,
@@ -70,7 +70,7 @@ function authenticateTravis(github_token, cb) {
     res.on('data', function (chunk) { body += chunk; });
     res.on('end', function() {
       var tokenString = body.match(':"(.*)"}');
-      cb(null, tokenString[1]); //this is our Travis API token
+      //cb(null, tokenString[1]); //this is our Travis API token
     });
   });
 
@@ -91,13 +91,14 @@ app.all('*', function (req, res, next) {
 
 app.get('/authenticate/:code', function(req, res) {
   console.log('authenticating code:' + req.params.code);
-  authenticateGitHub(req.params.code, function(err, token) {
+  authenticate(req.params.code, function(err, token) {
     var result = err || !token ? {"error": "bad_code"} : { "token": token };
     console.log(result);
     res.json(result);
   });
 });
 
+/*
 app.get('/auth/travis/:github_token', function(req, res) {
   authenticateTravis(req.params.github_token, function(err, access_token) {
     var result = err || !access_token ? {"error": "bad_github_token"} : { access_token };
@@ -105,6 +106,9 @@ app.get('/auth/travis/:github_token', function(req, res) {
     res.json(result);
   });
 });
+*/
+var port = process.env.PORT || config.port || 9999;
 
-var port_number = server.listen(process.env.PORT || 9999);
-app.listen(port_number);
+app.listen(port, null, function (err) {
+  console.log('Gatekeeper, at your service: http://localhost:' + port);
+});
